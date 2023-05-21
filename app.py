5639bb0583116a587a3c6ab22e6660fdb84c45f7
oleg_vl <@nitro-pc>
2023-05-21 16:49:25 +0300

    Code formatted

--- app.py
+++ app.py
@@ -5,71 +5,80 @@ import requests
 import sqlite3
 import os
 import sys
-#requests.packages.urllib3.disable_warnings() #отключить ошибку SSL-сертификата
 
-#Файл с данными для запроса к сайту
+# requests.packages.urllib3.disable_warnings() #отключить ошибку SSL-сертификата
+
+# Файл с данными для запроса к сайту
 from src import queries
 from src.panda import construct_df, construct_pivot, save_in_excel, save_in_sql
+
 
 class ReestrRequest(object):
     """Создание объекта для парсинга данных с реестра Роснедр https://rfgf.ru/ReestrLic/"""
 
     def __init__(self, filt: str):
-        #Создание объекта сессии:
-        #Путь к chain-сертификату сайта: 
-        cert = os.path.relpath('src/key.pem', os.getcwd())
+        # Создание объекта сессии:
+        # Путь к chain-сертификату сайта:
+        cert = os.path.relpath("src/key.pem", os.getcwd())
 
         self.session = requests.Session()
         self.session.verify = cert
 
-        #Переменная фильтра
+        # Переменная фильтра
         self.filt = filt
 
         self.url = queries.url
         self.headers = queries.headers
 
-        #Подстановка нужного фильтра в POST запрос
+        # Подстановка нужного фильтра в POST запрос
         self.json_data = queries.json_data
-        self.json_data['RawOlapSettings']['measureGroup']['filters'][0][0]['selectedFilterValues'] = [filt]
+        self.json_data["RawOlapSettings"]["measureGroup"]["filters"][0][0][
+            "selectedFilterValues"
+        ] = [filt]
 
         self.nested_data = {}
-        
 
     def get_record_count(self):
-        
         """
-        Метод для получения количества записей в базе данных Роснедр для создания запроса на полное количество данных. 
+        Метод для получения количества записей в базе данных Роснедр для создания запроса на полное количество данных.
         Возвращает int с количеством записей в базе для использования в методе get_data
         """
-        
-        #Данные для запроса 
-        self.json_data['RawOlapSettings']['lazyLoadOptions']['limit'] = 1
-        
-        response = self.session.post(self.url, headers=self.headers, json=self.json_data).json()
-        return int(response['result']['recordCount'])
 
+        # Данные для запроса
+        self.json_data["RawOlapSettings"]["lazyLoadOptions"]["limit"] = 1
+
+        response = self.session.post(
+            self.url, headers=self.headers, json=self.json_data
+        ).json()
+        return int(response["result"]["recordCount"])
 
     def get_data_from_reestr(self):
         """
-        Метод делает запросы к базе данных Роснедр. 
+        Метод делает запросы к базе данных Роснедр.
         Возращает плоский Python-словарь с данными.
         """
-        
-        #Получение количества записей в реестре. Создание заголовка запроса для получения всех записей выгрузки
-        num = self.get_record_count() 
-        self.json_data['RawOlapSettings']['lazyLoadOptions']['limit'] = num
-        
-        #Запрос для получения всех записей выгрузки    
-        response = self.session.post(self.url, headers=self.headers, json=self.json_data).json()
-        
-        #Подготовка данных
-        response['result']['data']['cols'][16] = ['Дата.1']
-        response['result']['data']['cols'][18] = ['Дата.2']
-        
-        #Python-словарь из данных запроса для загрузки его в панду или сохранения
-        self.nested_data = {k:v for n, v in enumerate(response['result']['data']['values']) for k in response['result']['data']['cols'][n]}
+
+        # Получение количества записей в реестре. Создание заголовка запроса для получения всех записей выгрузки
+        num = self.get_record_count()
+        self.json_data["RawOlapSettings"]["lazyLoadOptions"]["limit"] = num
+
+        # Запрос для получения всех записей выгрузки
+        response = self.session.post(
+            self.url, headers=self.headers, json=self.json_data
+        ).json()
+
+        # Подготовка данных
+        response["result"]["data"]["cols"][16] = ["Дата.1"]
+        response["result"]["data"]["cols"][18] = ["Дата.2"]
+
+        # Python-словарь из данных запроса для загрузки его в панду или сохранения
+        self.nested_data = {
+            k: v
+            for n, v in enumerate(response["result"]["data"]["values"])
+            for k in response["result"]["data"]["cols"][n]
+        }
         return self.nested_data
-        
+
     def create_df(self):
         """
         Метод создает пандасовский датафрейм из выгруженного словаря self.nested_data
@@ -77,20 +86,23 @@ class ReestrRequest(object):
         self.dataframe = construct_df(self.get_data_from_reestr())
         return self.dataframe
 
+
 def path_to_desktop():
-    #Функция для определения пути сохранения файла на рабочий стол в зависимости от ОС
-    
+    # Функция для определения пути сохранения файла на рабочий стол в зависимости от ОС
+
     ostype = sys.platform
-    if 'win' in ostype:
-        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+    if "win" in ostype:
+        desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
     else:
-        desktop = os.path.expanduser('~')
+        desktop = os.path.expanduser("~")
     return desktop
+
 
 dpath = path_to_desktop()
 
+
 class PivotMatrix(ReestrRequest):
-    #Класс объекта, который преобразуется в pivot для создания матрицы год-прерыдущая лицензия
+    # Класс объекта, который преобразуется в pivot для создания матрицы год-прерыдущая лицензия
 
     def __init__(self, filt):
         super().__init__(filt)
@@ -103,33 +115,35 @@ class PivotMatrix(ReestrRequest):
 
         return self.piv_df
 
-class ReestrDatabase(ReestrRequest):
-    #Класс для сохранения в базу данных sqlite3
 
-    def __init__(self, filt:str):
+class ReestrDatabase(ReestrRequest):
+    # Класс для сохранения в базу данных sqlite3
+
+    def __init__(self, filt: str):
         super().__init__(filt)
         self.filt = filt
-        name = [k for k,v in queries.lfilt.items() if self.filt == v] #извлечение ключа словаря фильтра для названия таблицы
+        name = [
+            k for k, v in queries.lfilt.items() if self.filt == v
+        ]  # извлечение ключа словаря фильтра для названия таблицы
 
-        #Create connection to DB
-        conn = sqlite3.Connection('reestr_database.db', detect_types=1)
-        
-        #Request and create dataframe
+        # Create connection to DB
+        conn = sqlite3.Connection("reestr_database.db", detect_types=1)
+
+        # Request and create dataframe
         self.create_df()
 
         save_in_sql(*name, self.dataframe, conn)
-    
 
 
 if __name__ == "__main__":
-#Запуск для автоматической выгрузки по нефти или дебаггинга
+    # Запуск для автоматической выгрузки по нефти или дебаггинга
 
-    filepath = os.path.join(dpath, 'reestr_pivot.xlsx') 
-    
-    r1 = ReestrRequest(queries.lfilt['oil'])
-    save_in_excel(filepath, r1.create_df(), 'reestr_oil')
+    filepath = os.path.join(dpath, "reestr_pivot.xlsx")
 
-    r2 = PivotMatrix(queries.lfilt['oil'])
-    save_in_excel(filepath, r2.pivoting(), 'Licensoins_pivot')
+    r1 = ReestrRequest(queries.lfilt["oil"])
+    save_in_excel(filepath, r1.create_df(), "reestr_oil")
 
-    r3 = ReestrDatabase(queries.lfilt['oil'])
+    r2 = PivotMatrix(queries.lfilt["oil"])
+    save_in_excel(filepath, r2.pivoting(), "Licensoins_pivot")
+
+    r3 = ReestrDatabase(queries.lfilt["oil"])
--- src/panda.py
+++ src/panda.py
@@ -6,162 +6,276 @@ import os
 
 def construct_df(nested_data: dict):
     """
-    Функция для создания пандас-датафрейма и обработки словаря, полученного после запроса к реестру. Возвращает датафрейм для сохранения. 
+    Функция для создания пандас-датафрейма и обработки словаря, полученного после запроса к реестру. Возвращает датафрейм для сохранения.
     """
-    #Переменные столбцов и типов для возврата датафрейма.
+    # Переменные столбцов и типов для возврата датафрейма.
     cols = [
-            'Дата лицензии',
-            'СубъектРФ','Вид полезного ископаемого','Наименование участка недр',
-            'Сведения о пользователе недр','INN','Наименоваение недропользователя','prev_owner',
-            'Last','Year',
-            'Координаты','N', 'E',
-            'Сведения о переоформлении лицензии на пользование недрами','Ранее выданные лицензии',
-            'prev_lic', 'prev_date',
-            'forw_lic', 'forw_date',
-            ]
+        "Дата лицензии",
+        "СубъектРФ",
+        "Вид полезного ископаемого",
+        "Наименование участка недр",
+        "Сведения о пользователе недр",
+        "INN",
+        "Наименоваение недропользователя",
+        "prev_owner",
+        "Last",
+        "Year",
+        "Координаты",
+        "N",
+        "E",
+        "Сведения о переоформлении лицензии на пользование недрами",
+        "Ранее выданные лицензии",
+        "prev_lic",
+        "prev_date",
+        "forw_lic",
+        "forw_date",
+    ]
     types = {
-            'Дата лицензии':'datetime64[D]',
-            'Last':'bool',
-            'N':'str', 'E':'str',
-            'prev_lic':'str', 'prev_date':'datetime64[D]',
-            'forw_lic':'str', 'forw_date':'datetime64[D]',
-            'Наименование участка недр':'str', 'Вид полезного ископаемого':'str',
-            'СубъектРФ':'str', 'INN':'str', 'Year':'int',
+        "Дата лицензии": "datetime64[D]",
+        "Last": "bool",
+        "N": "str",
+        "E": "str",
+        "prev_lic": "str",
+        "prev_date": "datetime64[D]",
+        "forw_lic": "str",
+        "forw_date": "datetime64[D]",
+        "Наименование участка недр": "str",
+        "Вид полезного ископаемого": "str",
+        "СубъектРФ": "str",
+        "INN": "str",
+        "Year": "int",
+    }
+
+    df = (
+        pd.DataFrame(nested_data)
+        .rename(
+            columns={
+                "Дата": "Дата лицензии",
+                "Дата.1": "Дата прекращения лицензии",
+                "Дата.2": "Дата окончания лицензии",
+                "Географические координаты угловых точек участка недр, верхняя и нижняя границы участка недр": "Координаты",
+                "Наименование органа, выдавшего лицензию": "Орган",
+                "Сведения о реестровых записях в отношении ранее выданных лицензий на пользование соответствующим участком недр": "Ранее выданные лицензии",
+                "Реквизиты приказа о прекращении права пользования недрами, приостановлении или ограничении права пользования недрами": "Реквизиты прекращения, приостановления, ограничения",
+                "Наименование участка недр, предоставленного в пользование по лицензии, кадастровый номер месторождения или проявления полезных ископаемых в ГКМ": "Наименование участка недр",
+                "Наименование субъекта Российской Федерации или иной территории, на которой расположен участок недр": "СубъектРФ",
             }
-    
+        )
+        .set_index("Государственный регистрационный номер")
+    )
 
-    df = pd.DataFrame(nested_data).rename(columns={
-                'Дата':'Дата лицензии',
-                'Дата.1':'Дата прекращения лицензии',
-                'Дата.2':'Дата окончания лицензии',
-                'Географические координаты угловых точек участка недр, верхняя и нижняя границы участка недр':'Координаты',
-                'Наименование органа, выдавшего лицензию':'Орган',
-                'Сведения о реестровых записях в отношении ранее выданных лицензий на пользование соответствующим участком недр':'Ранее выданные лицензии',
-                'Реквизиты приказа о прекращении права пользования недрами, приостановлении или ограничении права пользования недрами': 'Реквизиты прекращения, приостановления, ограничения',
-                'Наименование участка недр, предоставленного в пользование по лицензии, кадастровый номер месторождения или проявления полезных ископаемых в ГКМ':'Наименование участка недр',
-                'Наименование субъекта Российской Федерации или иной территории, на которой расположен участок недр':'СубъектРФ'
-                }).set_index('Государственный регистрационный номер')
+    # выделение столбца ГОД
+    df["Year"] = pd.to_datetime(
+        df["Дата лицензии"], format="%Y-%m-%d", yearfirst=True
+    ).dt.year  # Год лицензии
+    df["Last"] = np.where(
+        df["Сведения о переоформлении лицензии на пользование недрами"].isna(),
+        True,
+        False,
+    )  # Условный столбец последняя лицензия
 
-    #выделение столбца ГОД
-    df['Year'] = pd.to_datetime(df['Дата лицензии'], format='%Y-%m-%d', yearfirst=True).dt.year #Год лицензии
-    df['Last'] = np.where(df['Сведения о переоформлении лицензии на пользование недрами'].isna(), True, False) #Условный столбец последняя лицензия
+    # STEP INN-----------
+    # Извлечение ИНН:
+    # Паттерны regrex
+    pattern_for_inn = "([\d{10}|\d{12}]+)"
+    pattern_for_replace_inn = "(\(ИНН.*\)$)"
 
-    #STEP INN-----------
-    #Извлечение ИНН:
-    #Паттерны regrex
-    pattern_for_inn = '([\d{10}|\d{12}]+)'
-    pattern_for_replace_inn = '(\(ИНН.*\)$)'
-        
-    df['INN'] = df['Сведения о пользователе недр'].str.extract(pattern_for_inn)
-    df['INN'] = df['INN'].astype(str, errors='ignore').str.replace('.0', '', regex=False)
-    df['Наименоваение недропользователя'] = df['Сведения о пользователе недр'].replace(pattern_for_replace_inn, '', regex=True).str.strip()
+    df["INN"] = df["Сведения о пользователе недр"].str.extract(pattern_for_inn)
+    df["INN"] = (
+        df["INN"].astype(str, errors="ignore").str.replace(".0", "", regex=False)
+    )
+    df["Наименоваение недропользователя"] = (
+        df["Сведения о пользователе недр"]
+        .replace(pattern_for_replace_inn, "", regex=True)
+        .str.strip()
+    )
 
-    #STEP PREV_FORW_LIC-----------
-    #Извлечение номеров предыдущих и будущих:
-    pattern_for_lic = '([А-Я]+[0-9]+[-А-Я]+)' #Паттерн для извлечения номера ранее выданной лицензии
-    pattern_for_date = '([\d\.]+$)' #Паттерн для извлечения даты ранее выданной лицензии
+    # STEP PREV_FORW_LIC-----------
+    # Извлечение номеров предыдущих и будущих:
+    pattern_for_lic = (
+        "([А-Я]+[0-9]+[-А-Я]+)"  # Паттерн для извлечения номера ранее выданной лицензии
+    )
+    pattern_for_date = (
+        "([\d\.]+$)"  # Паттерн для извлечения даты ранее выданной лицензии
+    )
 
-    #Извлечение данных предыдущих лицензий
-    df['prev_lic'] = df['Ранее выданные лицензии'].str.extract(pattern_for_lic)
-    df['prev_date'] = df['Ранее выданные лицензии'].str.extract(pattern_for_date)
-    df['prev_date'] = pd.to_datetime(df['prev_date'], format='%d.%m.%Y', errors='ignore', dayfirst=True)
+    # Извлечение данных предыдущих лицензий
+    df["prev_lic"] = df["Ранее выданные лицензии"].str.extract(pattern_for_lic)
+    df["prev_date"] = df["Ранее выданные лицензии"].str.extract(pattern_for_date)
+    df["prev_date"] = pd.to_datetime(
+        df["prev_date"], format="%d.%m.%Y", errors="ignore", dayfirst=True
+    )
 
-    #Извлечение данных будущих лицензий
-    df['forw_lic'] = df['Сведения о переоформлении лицензии на пользование недрами'].str.extract(pattern_for_lic)
-    df['forw_date'] = df['Сведения о переоформлении лицензии на пользование недрами'].str.extract(pattern_for_date) 
-    df['forw_date'] = pd.to_datetime(df['forw_date'], format='%d.%m.%Y', errors='ignore', dayfirst=True)
+    # Извлечение данных будущих лицензий
+    df["forw_lic"] = df[
+        "Сведения о переоформлении лицензии на пользование недрами"
+    ].str.extract(pattern_for_lic)
+    df["forw_date"] = df[
+        "Сведения о переоформлении лицензии на пользование недрами"
+    ].str.extract(pattern_for_date)
+    df["forw_date"] = pd.to_datetime(
+        df["forw_date"], format="%d.%m.%Y", errors="ignore", dayfirst=True
+    )
 
-    #STEP COORDS-----------
-    #Извлечение координат:
-    #Паттерн regrex
-    pattern_coord = '(?P<N>\d*°\d*\'[\d\.]+\"N)(?: *)(?P<E>[\d-]*°\d*\'[\d\.]+\"[E|Е])'
+    # STEP COORDS-----------
+    # Извлечение координат:
+    # Паттерн regrex
+    pattern_coord = "(?P<N>\d*°\d*'[\d\.]+\"N)(?: *)(?P<E>[\d-]*°\d*'[\d\.]+\"[E|Е])"
 
-    coords = df['Координаты'].str.extractall(pattern_coord, re.MULTILINE).sort_values(by='N', ascending=False).droplevel(1)
-    coords = coords[coords.index.duplicated(keep='first') == False]
+    coords = (
+        df["Координаты"]
+        .str.extractall(pattern_coord, re.MULTILINE)
+        .sort_values(by="N", ascending=False)
+        .droplevel(1)
+    )
+    coords = coords[coords.index.duplicated(keep="first") == False]
 
-    #Объединение датафрейма
-    df = pd.merge(df, coords, how='left', left_index=True, right_index=True)
+    # Объединение датафрейма
+    df = pd.merge(df, coords, how="left", left_index=True, right_index=True)
 
+    # STEP PREV_OWNER-----------
+    # Добавление столбца предыдущего владельца лицензии
+    df = df.loc[~df.index.duplicated(keep="last")]  # Убираем дубликаты из индекса
 
-    #STEP PREV_OWNER-----------
-    #Добавление столбца предыдущего владельца лицензии
-    df = df.loc[~df.index.duplicated(keep='last')] #Убираем дубликаты из индекса
-            
-    prev_owner_df = df.reset_index().set_index('forw_lic')
+    prev_owner_df = df.reset_index().set_index("forw_lic")
     prev_owner_df = prev_owner_df[prev_owner_df.index.notnull()]
-    prev_owner_df = prev_owner_df[~prev_owner_df.index.duplicated(keep='last')]['Наименоваение недропользователя'].rename('prev_owner')
-            
-    #Присоединение столбца с данными о предыдущих владельцах 
-    df = pd.concat([df,prev_owner_df], axis=1, join='outer').rename_axis('Государственный регистрационный номер')
-    df = df[df['Дата лицензии'].notna()]
+    prev_owner_df = prev_owner_df[~prev_owner_df.index.duplicated(keep="last")][
+        "Наименоваение недропользователя"
+    ].rename("prev_owner")
 
+    # Присоединение столбца с данными о предыдущих владельцах
+    df = pd.concat([df, prev_owner_df], axis=1, join="outer").rename_axis(
+        "Государственный регистрационный номер"
+    )
+    df = df[df["Дата лицензии"].notna()]
 
-    #STEP PREV_NAMES-----------
+    # STEP PREV_NAMES-----------
     for i in range(100):
-        names_df = df.reset_index().set_index('prev_lic')
+        names_df = df.reset_index().set_index("prev_lic")
         names_df = names_df[names_df.index.notnull()]
 
-        #Лист лицензий из names_df:
+        # Лист лицензий из names_df:
         lst = names_df.index.unique().tolist()
-        #Лист лицензий из основного ДФ у которых нет названий но есть названия в names_df:
-        lst2 = df.index[df.index.isin(lst) & df['Наименование участка недр'].isna()].tolist()
+        # Лист лицензий из основного ДФ у которых нет названий но есть названия в names_df:
+        lst2 = df.index[
+            df.index.isin(lst) & df["Наименование участка недр"].isna()
+        ].tolist()
 
-        #Замена значений названий:
-        df.loc[df.index.isin(lst2),['Наименование участка недр']] = names_df.loc[names_df.index.isin(lst2)]
+        # Замена значений названий:
+        df.loc[df.index.isin(lst2), ["Наименование участка недр"]] = names_df.loc[
+            names_df.index.isin(lst2)
+        ]
 
-    df = (df[cols].astype(dtype=types)
-          .where(~df.isnull(), '') #заменить значения NaN на пустые для выгрузки в эксель
-          )
+    df = (
+        df[cols]
+        .astype(dtype=types)
+        .where(
+            ~df.isnull(), ""
+        )  # заменить значения NaN на пустые для выгрузки в эксель
+    )
     return df
 
 
 def construct_pivot(df: pd.DataFrame) -> pd.DataFrame:
-
     """
     Функция для создания матрицы ключей для связки с ГБЗ. Создает плоскую таблицу с столбцами год и номерами предыдущих лицензий.
 
     :return pd.DataFrame: датафрейм для сохранения в эксель
     """
-    
-    #Создание датафрейма с номерами послединих лицензий
-    df_last = df[['Наименование участка недр','Государственный регистрационный номер', 'Дата лицензии', 'Year']][df.Last == True]
-    df_last['prev_lic'] = df_last['Государственный регистрационный номер']
 
-    #Создание датафрейма для присоединения к мейну
-    df_last_pivoted = df_last.pivot(index=['Наименование участка недр','Государственный регистрационный номер', 'Дата лицензии'], columns=['Year'])['prev_lic'].sort_values(by = ['Year'], axis=1, ascending=False).bfill(axis = 1).ffill(axis = 1)
+    # Создание датафрейма с номерами послединих лицензий
+    df_last = df[
+        [
+            "Наименование участка недр",
+            "Государственный регистрационный номер",
+            "Дата лицензии",
+            "Year",
+        ]
+    ][df.Last == True]
+    df_last["prev_lic"] = df_last["Государственный регистрационный номер"]
 
-    #Создание основного датафрейма для лицензий
-    df_main = df[['Наименование участка недр','Государственный регистрационный номер','Дата лицензии', 'Year', 'prev_lic']].sort_values('Year', ascending=False)
+    # Создание датафрейма для присоединения к мейну
+    df_last_pivoted = (
+        df_last.pivot(
+            index=[
+                "Наименование участка недр",
+                "Государственный регистрационный номер",
+                "Дата лицензии",
+            ],
+            columns=["Year"],
+        )["prev_lic"]
+        .sort_values(by=["Year"], axis=1, ascending=False)
+        .bfill(axis=1)
+        .ffill(axis=1)
+    )
 
-    #добавление "_" к значению
-    df_main['prev_lic'] = df_main['prev_lic'].apply(lambda x: '_' + x if type(x) is str else np.nan)
+    # Создание основного датафрейма для лицензий
+    df_main = df[
+        [
+            "Наименование участка недр",
+            "Государственный регистрационный номер",
+            "Дата лицензии",
+            "Year",
+            "prev_lic",
+        ]
+    ].sort_values("Year", ascending=False)
 
-    #Создание пивот-датафрейма с столбцами в убывающем порядке 
-    df_pivoted = df_main.pivot(index=['Наименование участка недр','Государственный регистрационный номер', 'Дата лицензии'], columns=['Year'])['prev_lic'].sort_values(by = ['Year'], axis=1, ascending=False).ffill(axis=1)
+    # добавление "_" к значению
+    df_main["prev_lic"] = df_main["prev_lic"].apply(
+        lambda x: "_" + x if type(x) is str else np.nan
+    )
 
-    #Объединение датафреймов main и last. Датафрейм last это дополнительная строка последнеей записи в реестре
-    df_concat = pd.concat([df_pivoted, df_last_pivoted]).sort_index(level=[0,2])
+    # Создание пивот-датафрейма с столбцами в убывающем порядке
+    df_pivoted = (
+        df_main.pivot(
+            index=[
+                "Наименование участка недр",
+                "Государственный регистрационный номер",
+                "Дата лицензии",
+            ],
+            columns=["Year"],
+        )["prev_lic"]
+        .sort_values(by=["Year"], axis=1, ascending=False)
+        .ffill(axis=1)
+    )
+
+    # Объединение датафреймов main и last. Датафрейм last это дополнительная строка последнеей записи в реестре
+    df_concat = pd.concat([df_pivoted, df_last_pivoted]).sort_index(level=[0, 2])
 
     return df_concat
 
-def save_in_sql(table, df:pd.DataFrame, connect:str):
-    df = (df.reset_index()
-          .astype(dtype='str')
-          .to_sql(name=table, con=connect, if_exists='replace')
-        )
+
+def save_in_sql(table, df: pd.DataFrame, connect: str):
+    df = (
+        df.reset_index()
+        .astype(dtype="str")
+        .to_sql(name=table, con=connect, if_exists="replace")
+    )
 
 
-def save_in_excel(path: str, dataframe_to_save:pd.DataFrame, name_for_sheet: str):
+def save_in_excel(path: str, dataframe_to_save: pd.DataFrame, name_for_sheet: str):
     """
     Функция для сохранения в определенный лист экселя
-    path - путь для сохранения 
+    path - путь для сохранения
     df - датафрейм для сохранения
     name_for_sheet - название листа
     """
 
     if os.path.exists(path):
-        with pd.ExcelWriter(path, mode='a', if_sheet_exists='replace') as writer:
-            dataframe_to_save.to_excel(writer, sheet_name=name_for_sheet, merge_cells=False, freeze_panes=(1,0), na_rep='')
+        with pd.ExcelWriter(path, mode="a", if_sheet_exists="replace") as writer:
+            dataframe_to_save.to_excel(
+                writer,
+                sheet_name=name_for_sheet,
+                merge_cells=False,
+                freeze_panes=(1, 0),
+                na_rep="",
+            )
 
     else:
-        dataframe_to_save.to_excel(path, sheet_name=name_for_sheet, merge_cells=False, freeze_panes=(1,0), na_rep='') 
+        dataframe_to_save.to_excel(
+            path,
+            sheet_name=name_for_sheet,
+            merge_cells=False,
+            freeze_panes=(1, 0),
+            na_rep="",
+        )
